6aa46121934aaddf2e81716745886fc59d3cabbb
kuta-kings <kutasofficial@gmail.com>
2023-02-16 13:23:34 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line with puts function
- * return: 0 (success)
+ * main - prining a line using puts function
+ * Return: 0 (Success)
  */
 int main(void)
 {
