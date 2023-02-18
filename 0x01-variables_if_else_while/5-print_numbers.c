b49bf107d6a4957441a8c2ff7499bc8ef3466ce8
kuta-kings <kutasofficial@gmail.com>
2023-02-17 21:40:10 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - A program that runs numbers less than ten in base ten
+ * Return: 0 (Seuccess)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+
+	printf("\n");
+
+	return (0);
+}
